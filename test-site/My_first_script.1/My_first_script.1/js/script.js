b6415bc5bcc6f46bf5b0f76ef06bba848459f0bc
Gast Nataliia <nataliia.gast@gmail.com>
2023-03-31 20:40:27 +0200

    Projekt

---
+++ test-site/My_first_script.1/My_first_script.1/js/script.js
@@ -0,0 +1,13 @@
+
+
+// const r = 12;
+// const g = 34;
+// const b = 14;
+
+// console.log("rgb(" + "r" + "," + "g" + "," + "b" + ")");
+// console.log('rgb(${r},${g},${b})');
+
+let num = 5;
+console.log(num + 5);
+console.log(+num);
+
