59814d9902a2fa688b8f4502a8b07fa3cd785988
Gast Nataliia <nataliia.gast@gmail.com>
2023-04-23 18:59:44 +0200

    Project

---
+++ test-site/Frontend_2023/Lesson-20.04/Lesson-20.04/Homework-20.04/js/script.js
@@ -0,0 +1,52 @@
+// 1)  https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style
+// 2)  https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+// 3)  https://www.javatpoint.com/javascript-classlist
+
+// Задача:
+// В js объявляйте создайте массив с ссылками на картинки. На основе этого массива формируйте множество маленьких картинок в верхней части интерфейса.Пользователь нажимая на маленькие картинки видит их отображение в большем размере(только ту картинку, на которую он нажимал).
+
+const image = [
+	"./img/alec-favale-Ivzo69e18nk-unsplash.jpg",
+
+	"./img/baptist-standaert-mx0DEnfYxic-unsplash.jpg",
+
+	"./img/celine-sayuri-tagami-2s6ORaJY6gI-unsplash.jpg",
+
+	"./img/cody-board-tnNVJd_nrw8-unsplash.jpg",
+
+	"./img/jamie-street-UtrE5DcgEyg-unsplash.jpg",
+
+	"./img/jamie-street-wcO2PWLuQ3U-unsplash.jpg",
+
+	"./img/oscar-sutton-yihlaRCCvd4-unsplash.jpg",
+
+	"./img/kevin-turcios-rls2bfqYh8E-unsplash.jpg",
+
+	"./img/mike-burke-gxyfJQg7Lno-unsplash.jpg",
+
+	"./img/kieran-white-NKN25UfGfkQ-unsplash.jpg",
+];
+const div = document.createElement("div");
+div.classList.add("wrapper");
+document.body.append(div);
+const textOfAlt = "dog img";
+for (let i = 0; i < image.length; i++) {
+	const img = document.createElement("img");
+	img.setAttribute("src", image[i]);
+	img.setAttribute("alt", textOfAlt);
+	img.classList.add("image");
+	const p = document.createElement("p");
+	p.append(img);
+	p.classList.add("wrap");
+	div.append(p);
+	img.onclick = () => {
+		if (img.classList[0] === "image")
+			img.classList.toggle("big");
+	};
+};
+
+
+
+
+
+
--- test-site/Frontend_2023/Lesson_18.04/Lesson_18.04/js/script.js
+++ test-site/Frontend_2023/Lesson_18.04/Lesson_18.04/js/script.js
@@ -2,41 +2,40 @@
 // function expression
 // arrow function
 
-let x = 10;
-console.log(x);
-
 
 // function decloration //hositing (function dekloration, var)
-hi();
-function hi() {
-	console.log("Hello!");
-};
-hi();
+// hi();
+// function hi() {
+// 	console.log("Hello!");
+// };
+// hi();
 
 
 // function expression
-const hello = function () {
-	console.log('Hello!');
-};
-hello();
+// const hello = function () {
+// 	console.log('Hello!');
+// };
+// hello();
 
 
 // arrow function
-const hel = () => {
-	console.log("Hello!");
-};
-const helLo = () => console.log("Hello!");
-const helReturn = () => "Hello";
+// const hel = () => {
+// 	console.log("Hello!");
+// };
+// const helLo = () => console.log("Hello!");
+// const helReturn = () => "Hello";
 
 
 //callback funtion
 // Эта функция которую мы передаём другой функции в виде аргумента.
 // Переданная функция в аргументах это - callback function.
+
 const function1 = function () {
 	console.log("This is function1");
 };
-const function2 = function () {
+const function2 = function (callback) {
 	console.log("This is function2");
 	return callback();
 };
+
 function2(function1);
